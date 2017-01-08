850fd1416c3dfc033a2619b091b0ef108b86c329
sega <stanpaponov@gmail.com>
2017-01-08 18:48:14 +0300

    Add countdown functionality

---
+++ app/components/Clock.jsx
@@ -0,0 +1,37 @@
+var React = require('react');
+
+var Clock = React.createClass({
+    getDefaultProps: function () {
+        totalSeconds: 0
+    },
+    propTypes: {
+        totalSeconds: React.PropTypes.number
+    },
+    formatSeconds: function (totalSecons) {
+        var seconds = totalSecons % 60;
+        var minutes = Math.floor(totalSecons / 60);
+
+        if (seconds < 10) {
+            seconds = '0' + seconds;
+        }
+
+        if (minutes < 10) {
+            minutes = '0' + minutes;
+        }
+
+        return minutes + ':' + seconds;
+    },
+    render: function () {
+        var {totalSeconds} = this.props;
+
+        return (
+        <div className="clock">
+            <span className="clock-text">
+                {this.formatSeconds(totalSeconds)}
+            </span>
+        </div>
+        );
+    }
+});
+
+module.exports = Clock;
